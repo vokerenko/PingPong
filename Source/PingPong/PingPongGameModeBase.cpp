5a86765919bf1f3468fb130de3aebe48da9fad96
Oleh Riabov <vokerenko@gmail.com>
2023-08-13 01:40:18 +0300

    Added spawning a ball on PostLogin

--- Source/PingPong/PingPongGameModeBase.cpp
+++ Source/PingPong/PingPongGameModeBase.cpp
@@ -25,3 +25,22 @@ AActor* APingPongGameModeBase::ChoosePlayerStart_Implementation(AController* Pla
 
 	return AGameModeBase::ChoosePlayerStart(Player);
 }
+
+void APingPongGameModeBase::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+	if (APongPlatformController* PC = Cast<APongPlatformController>(NewPlayer))
+	{
+		Players.Add(PC);
+	}
+	NumberOfPlayers++;
+	if (NumberOfPlayers == 2)
+	{
+		FTransform BallInitialTransform = FTransform(FVector(30.0f, -170.0f, -30.0f));
+		if (AActor* SpawnedBall = GetWorld()->SpawnActor(BallClass, &BallInitialTransform))
+		{
+			Ball = Cast<ABall>(SpawnedBall);
+		}
+		
+	}
+}
--- Source/PingPong/PingPongGameModeBase.h
+++ Source/PingPong/PingPongGameModeBase.h
@@ -2,9 +2,12 @@
 
 #pragma once
 
+#include "PongPlatformController.h"
+#include "Ball.h"
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "PingPongGameModeBase.generated.h"
+
 
 /**
  * 
@@ -15,5 +18,20 @@ class PINGPONG_API APingPongGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 public:
 	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
+
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+private:
+	UPROPERTY()
+	int NumberOfPlayers = 0;
+
+	UPROPERTY()
+	TArray<APongPlatformController*> Players;
+
+	UPROPERTY()
+	ABall* Ball;
+
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<ABall> BallClass;
 	
 };
