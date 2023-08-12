b9c6681d0374771412f362ca9a16d7c66d0c6ab3
Oleh Riabov <vokerenko@gmail.com>
2023-08-13 01:03:22 +0300

    Added base c++ classes

---
+++ Source/PingPong/PongPlatformController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PongPlatformController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONG_API APongPlatformController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
